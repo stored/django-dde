f53c08597fc9435cb55c3833258b211dd73ea70a
Nilton Teixeira <nilton.teixeira@stored.com.br>
2018-03-27 16:01:08 -0300

    Created base FakeApp

---
+++ tests/fixtures/fake_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class FakeModel(models.Model):
+
+    name = models.CharField('name', max_length=100)
+    email = models.EmailField()
+    info_1 = models.CharField(max_length=40)
+    info_2 = models.DateTimeField()
