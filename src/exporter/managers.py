b4960301bf10a19ec7198cc60c498a81852c8549
rafacassau <rafa.cassau@gmail.com>
2018-03-28 16:23:25 -0300

    fix

--- src/exporter/managers.py
+++ src/exporter/managers.py
@@ -32,6 +32,9 @@ class ExporterManager(models.Manager):
 
         obj.query.save(f'{obj.uuid}_queryset.pkl', query_file)
 
+        import ipdb
+        ipdb.set_trace()
+
         task_process.delay(obj.id)
 
         return obj
