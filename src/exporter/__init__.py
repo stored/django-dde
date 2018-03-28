c7f9e957f894bfb0968bf5349360189b65e84b13
Nilton Teixeira <nilton.teixeira@stored.com.br>
2018-03-28 15:09:53 -0300

    Celery issues created

--- src/exporter/__init__.py
+++ src/exporter/__init__.py
@@ -1,1 +1,3 @@
 from .celery_app import app
+
+__all__ = ['app']
--- tests/fixtures/fake_app/__init__.py
+++ tests/fixtures/fake_app/__init__.py
@@ -1,1 +1,5 @@
 default_app_config = 'tests.fake_app.apps.FakeApptestConfig'
+
+from exporter.celery_app import app
+
+__all__ = ['app']
