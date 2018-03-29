59b87bbd83aebbcd92ea79d66044f2f361cb095e
Nilton Teixeira <nilton.teixeira@stored.com.br>
2018-03-29 15:24:15 -0300

    Fixed celery_app branch confusion

--- src/exporter/celery_app.py
+++ src/exporter/celery_app.py
@@ -1,14 +1,6 @@
-import os
-from celery import Celery
+from django.apps import AppConfig
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+class ExporterConfig(AppConfig):
+    name = 'exporter'
 
-app = Celery('proj')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
