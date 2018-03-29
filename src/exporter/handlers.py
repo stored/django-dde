0150b88dc9fc324a7d45441bb6d2a2f599f605f8
Nilton Teixeira <nilton.teixeira@stored.com.br>
2018-03-29 15:03:42 -0300

    Created default storage handler

--- src/exporter/handlers.py
+++ src/exporter/handlers.py
@@ -4,34 +4,51 @@ import codecs
 
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+from django.utils.translation import ugettext as _
 
 from .utils import ExporterHelper
 
 
-def handler_default_storage(exporter, path_name):
-    """ Join the file_list (chunked files) into one then saves and return the saved path """
-    header = ExporterHelper.get_header(exporter.attrs)
+class FileHandler:
+    VALID_HANDLERS = {
+        'default_storage': default_storage
+    }
 
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.csv', delete=True, encoding="utf-8") as f:
-        writer = csv.writer(f, delimiter=str(';'), quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(header)
-        f.flush()
+    def __init__(self, exporter, pathname, target_storage='default_storage'):
+        self.path_name = pathname
+        self.exporter = exporter
+        self.target = self._get_file_storage(target_storage)
 
-        for chunk in exporter.chunks.all():
-            with default_storage.open(chunk.file.name) as temp_file:
-                reader = csv.reader(codecs.iterdecode(temp_file, 'utf-8'))
-                for row in reader:
-                    writer.writerow(row[0].split(';'))
-                    f.flush()
+    def proccess(self):
+        if self.target == 'default_storage':
+            self._proccess_default_storage()
 
-        # TODO search for better solution
-        # need to be a binary file, but csv.writerow can't write binary, try user DictWriter subclass
-        readble_file = open(f.name, 'rb').read()
+    def _get_file_storage(self, storage):
+        if storage not in self.VALID_HANDLERS:
+            raise KeyError(_("Invalid or unsupported storage"))
 
-        exporter.file.save(path_name, ContentFile(readble_file))
+        return self.VALID_HANDLERS[storage]
 
-    return exporter
+    def _proccess_default_storage(self):
+        """ Join the file_list (chunked files) into one then saves and return the saved path """
+        header = ExporterHelper.get_header(self.exporter.attrs)
 
+        with tempfile.NamedTemporaryFile(mode='w', suffix='.csv', delete=True, encoding="utf-8") as f:
+            writer = csv.writer(f, delimiter=str(';'), quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(header)
+            f.flush()
 
-def handle_s3():
-    pass
+            for chunk in self.exporter.chunks.all():
+                with default_storage.open(chunk.file.name) as temp_file:
+                    reader = csv.reader(codecs.iterdecode(temp_file, 'utf-8'))
+                    for row in reader:
+                        writer.writerow(row[0].split(';'))
+                        f.flush()
+
+            # TODO search for better solution
+            # need to be a binary file, but csv.writerow can't write binary, try user DictWriter subclass
+            readble_file = open(f.name, 'rb').read()
+
+            self.exporter.file.save(self.path_name, ContentFile(readble_file))
+
+        return self.exporter
