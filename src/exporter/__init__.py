2094e9157b5d7b5dcbb35c76c4c1742509e08e45
rafacassau <rafa.cassau@gmail.com>
2018-03-29 11:58:52 -0300

    remove custom celery app, remove duplicated settings file, and improve project structure

--- src/exporter/__init__.py
+++
@@ -1,3 +0,0 @@
-from .celery_app import app
-
-__all__ = ['app']
--- tests/fixtures/fake_app/apps.py
+++ tests/fixtures/fake_app/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from celery import Celery
+
+
+app = Celery(__name__)
 
 
 class FakeApptestConfig(AppConfig):
     name = 'tests.fixtures.fake_app'
+
+    def ready(self):
+        app.config_from_object('django.conf:settings', namespace='CELERY')
+        app.autodiscover_tasks()
